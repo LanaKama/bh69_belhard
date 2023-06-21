170742f699faa1d982db43c81563aa40ca7c395b
CharnyshSv <sv.charnysh@gmail.com>
2023-06-21 19:04:01 +0300

    ///

---
+++ urok_3.py
@@ -0,0 +1,19 @@
+age = 34
+name = 'Alex'
+is_human = True
+
+# text = "Helle your age is human'
+
+text1 = 'Hello' + name + 'your age' + str(age) + 'is human' + str(is_human)
+#text2 = 'Hello $s your age %d is human %d' % (name, age, is_human)
+text3 = 'Hello {} your age {} is human'. format(name, age, is_human)
+text4 = f'Hello {name} your age {age * 2} is human {is_human}'
+
+# print(f'{name:*>10}')
+# print (f'{2 * 2=     }')
+# print(f'{name!r}')
+# print(f'{"привет"!a}')
+# print(f'{3.14159:.2f}')
+#print(f'{3.14159:.2f}')
+
+# print('\N{fire}')
