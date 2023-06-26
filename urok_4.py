efdbe85c1caa426ca1fbfba02331350dcd3ca87b
CharnyshSv <sv.charnysh@gmail.com>
2023-06-26 20:20:21 +0300

    /////

---
+++ urok_4.py
@@ -0,0 +1,10 @@
+# numbers = [2, 4, 5, 7, 8, 2, 6, 8]
+# words = ['hello', 'python', 'world']
+# print(words[1][2])
+# l = list('hello')
+# print(l)
+#
+# letters = list('hello')
+# numbers2 = [i ** 2 for i in range(1, 100)]
+# print(numbers2)
+
