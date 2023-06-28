715d50898cd397c9defa4fbd1dc059fcf97b48f9
CharnyshSv <sv.charnysh@gmail.com>
2023-06-28 21:30:07 +0300

    /////

---
+++ urok_5.py
@@ -0,0 +1,54 @@
+# depozit = input('enter sum:')
+# percent = input('%:')
+# vklad = depozit * (1 + percent/100)
+# for i in range():
+#     if vklad ==
+# print(vklad)
+
+
+# depozit = input('enter sum:')
+# percent = input('%:')
+# percent = percent / 100 + 1
+# target = depozit * 2
+# year = 0
+# while depozit < target:
+#     year += 1
+#     depozit *= percent
+#     print(year)
+
+from collections import Counter
+
+# amount = int(input('enter summ with peni:'))
+# coins = (25, 10, 5, 1)
+# coins_count = 0
+# for coin in coins:
+#     coins_count += amount // coin
+#     amount -= (amount // coin) * coin
+# print(coins_count)
+
+# numbers = [3, 1, 2, 2, 5, 7]
+# for number in numbers:
+#     if number % 2:
+#         numbers.remove(number)
+#         print(numbers)
+#
+# number = input('enter number:')
+# max_num = 0
+# for num in number:
+#     if int(num) > max_num:
+#         max_num = int(num)
+# print(max_num)
+
+data = [
+    {'name': 'Name1', 'age': 12}
+    {'name': 'Name2', 'age': 13}
+    {'name': 'Name3', 'age': 15}
+    {'name': 'Name4', 'age': 18}
+    {'name': 'Name5', 'age': 22}
+    {'name': 'Name6', 'age': 34}
+    {'name': 'Name7', 'age': 37}
+]
+
+
+
+
