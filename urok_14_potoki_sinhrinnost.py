084cfcf40e057cf28d62da199fd8421ab4a74865
CharnyshSv <sv.charnysh@gmail.com>
2023-08-02 19:27:02 +0300

    ///

---
+++ urok_14_potoki_sinhrinnost.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+from threading import (
+    current_thread,
+    main_thread,
+    Thread,
+    Timer,
+    Event,
+    Lock,
+    RLock,
+    Semaphore,
+    Barrier,
+    active_count,
+    get_ident,
+    local
+)
+
+from threading import enumerate as threading_enumerate
+
+def
+
+
